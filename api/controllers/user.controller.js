baf3c0b8864a006ded40fa29f3c592071afc8194
Hassan-Hammoud <hassanhammoud2000@gmail.com>
2024-07-03 16:40:01 +0300

    add delete user functionality

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -9,32 +9,6 @@ export const test = (req, res) => {
 };
 
 // Update User Information
-// export const updateUser = async (req, res, next) => {
-//   if (req.user.id !== req.params.id)
-//     return next(errorHandler(401, "You can only update your own account!"));
-//   try {
-//     if (req.body.password) {
-//       req.body.password = bcryptjs.hashSync(req.body.password, 10);
-//     }
-//     const updateUser = await User.findByIdAndUpdate(
-//       req.params.id,
-//       {
-//         $set: {
-//           username: req.body.username,
-//           email: req.body.email,
-//           password: req.body.password,
-//           avatar: req.body.avatar,
-//         },
-//       },
-//       { new: true }
-//     );
-//     const { password, ...rest } = updateUser._doc;
-
-//     res.status(200).json(rest);
-//   } catch (error) {
-//     next(error);
-//   }
-// };
 
 export const updateUser = async (req, res, next) => {
   if (req.user.id !== req.params.id)
@@ -60,6 +34,20 @@ export const updateUser = async (req, res, next) => {
     const { password, ...rest } = updatedUser._doc;
 
     res.status(200).json(rest);
+  } catch (error) {
+    next(error);
+  }
+};
+
+// Delete a user
+
+export const deleteUser = async (req, res, next) => {
+  if (req.user.id !== req.params.id)
+    return next(errorHandler(401, "You can only update your own account!"));
+  try {
+    await User.findByIdAndDelete(req.params.id);
+    res.clearCookie("access_token");
+    res.status(200).json("User has been deleted!");
   } catch (error) {
     next(error);
   }
