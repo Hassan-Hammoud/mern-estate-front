b43642f1ad8afcccff7ca0dcfc6809a6f491eb16
Hassan-Hammoud <hassanhammoud2000@gmail.com>
2024-07-07 18:46:48 +0300

    complete update listing functionality

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -10,6 +10,8 @@ export const createListing = async (req, res, next) => {
   }
 };
 
+// Delete a listing
+
 export const deleteListing = async (req, res, next) => {
   const listing = await Listing.findById(req.params.id);
 
@@ -27,6 +29,8 @@ export const deleteListing = async (req, res, next) => {
     next(error);
   }
 };
+
+// Update a listing
 
 export const updateListing = async (req, res, next) => {
   const listing = await Listing.findById(req.params.id);
@@ -46,6 +50,20 @@ export const updateListing = async (req, res, next) => {
     );
 
     res.status(200).json(updatedListing);
+  } catch (error) {
+    next(error);
+  }
+};
+
+// Get a Listing
+
+export const getListing = async (req, res, next) => {
+  try {
+    const listing = await Listing.findById(req.params.id);
+    if (!listing) {
+      return next(errorHandler(404, "Listing not found!"));
+    }
+    res.status(200).json(listing);
   } catch (error) {
     next(error);
   }
